59fc417dc1280c3f85f3d14346fc621f658de5ce
belmiro-kunga <belmirokunga@gmail.com>
2021-03-07 15:01:46 +0100

    PHP echo e print Statements
    A declaração echo do PHP
    A declaração de print do PHP

---
+++ PHP_echo_e_print_Statements/echo_e_print.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP echo e print Statements</h1>
+
+    <?php
+/*
+echo "<h2> php é divertido! </2> <br>";
+echo "<h2> php é divertido! </2> <br>";
+echo "<h2> php é divertido! </2> <br>";
+echo "<h2> php é divertido! </2> <br>";
+echo "<h2> php é divertido! </2> <br>";
+echo "<h2> php é divertido! </2> <br>";
+*/
+/*
+$txt1 = "aprenda php";
+$txt2 = "aprenda html";
+
+$x = 5;
+$y = 3;
+
+echo " <h2>" . $txt1 . "</h2>";
+echo "estudar o php é maravilhoso " . $txt2 . "<br>";
+echo $x + $y;
+*/
+print "<h2> php é divertido! </2> <br>";
+print "<h2> php é divertido! </2> <br>";
+
+$txt1 = "aprenda php";
+$txt2 = "aprenda html";
+
+$x = 5;
+$y = 3;
+
+print " <h2>" . $txt1 . "</h2>";
+print "estudar o php é maravilhoso " . $txt2 . "<br>";
+print $x + $y;
+    ?>
+</body>
+</html>
