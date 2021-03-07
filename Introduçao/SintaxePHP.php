1821a50508c1f4f37b34c0d73d451c632beb40e5
belmiro-kunga <belmirokunga@gmail.com>
2021-03-07 13:40:22 +0100

    Sintaxe PHP
    Sintaxe básica do PHP,
    Sensibilidade a maiúsculas e minúsculas de PHP

---
+++ Introduçao/SintaxePHP.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Sintaxe PHP</h1>
+   
+   <?php
+   /*
+    ECHO "OLA MUNDO <br>";
+    echo "OLA MUNDO <br>" ;
+    Echo "OLA MUNDO <br>";
+   */
+
+   $color = "vermelho";
+   echo "Minha cor é " . $color . "<br>";
+   echo "Minha cor é " . $Color . "<br>";
+   echo "Minha cor é " . $coLor . "<br>";
+
+   ?>
+
+
+
+
+
+</body>
+</html>
