372aa2be6a900c0d2da4e99aec8a579270f07ca0
belmiro-kunga <belmirokunga@gmail.com>
2021-03-13 13:56:33 +0100

    PHP Math

---
+++ PHP_Math/math.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Math</h1>
+
+    <?php
+    /*
+echo (pi());função retorna o valor de PI:
+
+echo "<br>";
+
+echo(min(0, 145, 20, -5, -200));
+
+echo "<br>";    As funções min()e max()podem 
+ser usadas para encontrar o valor mais baixo ou mais 
+alto em uma lista de argumentos:
+
+echo(max(0, 145, 20, -5, -200));
+
+
+echo "<br>";
+echo (abs(-34.19));A abs()função retorna o valor absoluto (positivo) de um número:
+echo "<br>";
+
+
+
+echo(sqrt(81));A sqrt()função retorna a raiz quadrada de um número:
+
+echo(round(0.60) . "<br>");A round()função arredonda um número de 
+ponto flutuante para o número inteiro mais próximo:
+echo(round(0.50) . "<br>");
+echo(round(0.49) . "<br>");
+echo(round(-4.40) . "<br>");
+echo(round(-4.60));
+
+
+echo(rand()); A rand()função gera um número aleatório:
+*/
+
+echo(rand(20, 50));
+
+
+
+
+?>
+
+
+
+    
+</body>
+</html>
