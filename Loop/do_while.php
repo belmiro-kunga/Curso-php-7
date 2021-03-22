d5399cdcee47ff0a9f2774e160211cfefafef092
belmiro-kunga <belmirokunga@gmail.com>
2021-03-22 13:57:16 +0100

    for

---
+++ Loop/do_while.php
@@ -0,0 +1,18 @@
+<?php
+/*
+$x = 1;
+
+do{
+    echo "O número é $x <br>";
+        $x--;
+}while($x <= 5);
+*/
+
+$y = 6;
+
+do{
+    print "O numero é $y <br>";
+$y++;
+
+
+}while($y <= 6);
---
+++ Loop/loop.php
@@ -0,0 +1,24 @@
+<?php
+/*
+$x = 1; - Inicialize o contador de loop ($x) e defina o valor inicial para 1
+$x <= 5 - Continue o loop enquanto $x for menor ou igual a 5
+$x++; - Aumentar o valor do contador de loop em 1 para cada iteração
+$x = 0;
+
+while( $x  <= 5){
+    echo " o numero é $x <br>";
+     $x++;
+}
+
+
+$x = 0; - Inicialize o contador de loop ($x) e defina o valor inicial para 0
+$x <= 100 - Continue o loop enquanto $x for menor ou igual a 100
+$x+=10; - Aumentar o valor do contador de loop em 10 para cada iteração
+$y = 0;
+while ($y <= 100){
+    echo "O numero é; $y <br>";
+
+    $y+=20;
+}
+
+*/
